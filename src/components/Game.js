49230327d526571f4bd64dd11350eb07216a1ca5
nkbtemmy <emmanuelnkubito2@gmail.com>
2021-01-26 00:18:53 +0200

    adding same components

--- src/components/Game.js
+++ src/components/Game.js
@@ -23,17 +23,43 @@ function calculateWinner(squares) {
 }
 
 export default class Game extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
+  constructor(props) {
+      super(props);
+      this.state = {
         history: [
-            {
+          {
             squares: Array(9).fill(null)
-            }
+          }
         ],
         stepNumber: 0,
         xIsNext: true
-        };
+      };
+    }
+
+    handleClick(i) {
+      const history = this.state.history.slice(0, this.state.stepNumber + 1);
+      const current = history[history.length - 1];
+      const squares = current.squares.slice();
+      if (calculateWinner(squares) || squares[i]) {
+        return;
+      }
+      squares[i] = this.state.xIsNext ? "X" : "O";
+      this.setState({
+        history: history.concat([
+          {
+            squares: squares
+          }
+        ]),
+        stepNumber: history.length,
+        xIsNext: !this.state.xIsNext
+      });
+    }
+
+    jumpTo(step) {
+      this.setState({
+        stepNumber: step,
+        xIsNext: (step % 2) === 0
+      });
     }
   render() {
     const history = this.state.history;
